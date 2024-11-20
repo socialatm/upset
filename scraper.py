c7edcbe6e550f5099307a445c5921412ddce4cd3
morello12 <mobclipz12@gmail.com>
2024-11-19 18:06:47 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,229 +7,217 @@ import re
 import datetime
 import sqlite3
 import time
-# created a list of links beforehand to iterate over
+import os
 
-# list to store of page links
-links = []
-alphabets = sorted(set(string.ascii_lowercase))
-
-# lists to store fighters info and stats
-# Basic information
-f_name = []
-record = []
-height = []
-weight = []
-reach = []
-stance = []
-dob = []
-last_fought = []
-
-# Career Statistics
-slpm = []
-stra = []
-sapm = []
-strd = []
-td = []
-tda = []
-tdd = []
-suba = []
+all_links = []
+location = []
+events = []
+f1 = []
+f2 = []
+winner = []
+f1_odds = []
+f2_odds = []
+label = []
+favourite = []
 
 def scrape_data():
-    for alpha in alphabets:
-        links.append("http://www.fightmetric.com/statistics/fighters?char=" + alpha + "&page=all")
+    # set up page to extract table
+    data = requests.get("https://www.betmma.tips/mma_betting_favorites_vs_underdogs.php?Org=1")
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-        # now that we have a list of links we need to iterate it with BeautifulSoup
+    # table with 98% width 
+    table = soup.find('table', {'width': "98%"})
+    # find all links in that table
+    links = table.find_all('a', href=True)
+
+    # append all links to a list 
     for link in links:
-        print(f"Currently on this link: {link}")
+        all_links.append("https://www.betmma.tips/"+link.get('href'))
+
+    # test for one use case
+    for link in all_links:
+        print(f"Now currently scraping link: {link}")
 
         data = requests.get(link)
         soup = BeautifulSoup(data.text, 'html.parser')
-        names = soup.find_all('a', {'class': 'b-link b-link_style_black'}, href=True)   
+        time.sleep(1)
+        # specific table with the information
+        rows = soup.find_all('table', {'cellspacing': "5"})
 
-        # list to store url page of fighters
-        fighters = []
+        for row in rows:
 
-        for name in names:
-            fighters.append(name['href'])
+            # check for draw, if draw, then skip
+            # dictionary of won and lost
+            odds = row.find_all('td', {'align': "center", 'valign': "middle"})
+            # to avoid taking in draws
+            if odds[0].text not in ['WON', 'LOST']:
+                continue
 
-        fighters = sorted(set(fighters))
+            # event name
+            h1 = soup.find("h1")
+            # location and date
+            h2 = soup.find("h2")
+            
+            events.append(h1.text)
+            location.append(h2.text)
 
-        for fighter in fighters:
-            data = requests.get(fighter)
-            soup = BeautifulSoup(data.text, 'html.parser')
-            time.sleep(2)
-            # fighter's name
-            n = soup.find('span', {'class': 'b-content__title-highlight'})
-            f_name.append(n.text.strip())
-            print(f"Scraping the following fighter: {n.text.strip()}")
+            odds_f1 = float(odds[2].text.strip(" @"))
+            odds_f2 = float(odds[3].text.strip(" @"))
 
-            # record
-            rec = soup.find('span', {'class': 'b-content__title-record'})
-            record.append(rec.text.strip().strip("Record: "))
+            f1_odds.append(odds_f1)
+            f2_odds.append(odds_f2)
 
-            # bio box
-            bio_box = soup.find('ul', {'class':'b-list__box-list'})
-            bio_box = bio_box.find_all('li')
+            # how to generate label
+            odds_dict = {}
+            odds_dict[odds[0].text] = odds_f1
+            odds_dict[odds[1].text] = odds_f2 
 
-            # bio box - Height
-            t = bio_box[0].text.strip().strip("Height:").strip()
-            h = re.sub('[\"\']', '', t)
-            height.append(h)
+            if odds_dict["WON"] > odds_dict["LOST"]:
+                label.append("Underdog")
+            else:
+                label.append("Favourite")
 
-            # bio box - Weight
-            w = bio_box[1].text.strip().strip("Weight:").strip().strip(" lbs.")
-            try:
-                weight.append(int(w))
-            except:
-                weight.append(w)
+            if odds_f1 > odds_f2:
+                favourite.append("f2")
+            else:
+                favourite.append("f1")
 
-            # bio box - reach
-            r = bio_box[2].text.strip().strip("Reach:").strip().strip("\"")
-            try:
-                reach.append(int(r))
-            except:
-                reach.append(r)
 
-            # bio box - stance
-            s = bio_box[3].text.strip().strip("STANCE:").strip()
-            stance.append(s)
-
-            # bio box - DOB
-            date = bio_box[4].text.strip().strip("DOB:").strip()
-            dob.append(date)
-
-            # career statistics
-            cs = soup.find('div', {'class': 'b-list__info-box-left clearfix'})
-            cs = cs.find_all('li')
-
-            # CS - SLPM
-            slpm.append(float(cs[0].text.strip().strip("SLpM:").strip()))
-
-            # CS - STRA
-            stra.append(int(cs[1].text.strip().strip("Str. Acc.:").strip().strip("%"))/100)
-
-            # CS - SAPM
-            sapm.append(float(cs[2].text.strip().strip("SApM:").strip()))
-
-            # CS - STRD
-            strd.append(int(cs[3].text.strip().strip("Str. Def:").strip().strip("%"))/100)
-
-            # CS - TD
-            td.append(float(cs[5].text.strip().strip("TD Avg.:").strip()))
-
-            # CS - TDA
-            tda.append(int(cs[6].text.strip().strip("TD Acc.:").strip().strip("%").strip("%"))/100)
-
-            # CS - TDD
-            tdd.append(int(cs[7].text.strip().strip("TD Def.:").strip().strip("%"))/100)
-
-            # CS - SUBA
-            suba.append(float(cs[8].text.strip().strip("Sub Avg.:").strip()))
-
-            # Last fought
-            try:
-                lf = soup.find('tr', {'class': 'b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click'})
-                last_fought.append(lf.find_all('p', {'class': 'b-fight-details__table-text'})[12].text.strip())
-            except:
-                last_fought.append('--')
-
+            fighters = row.find_all('a', attrs={'href': re.compile("^fighter_profile.php")})
+            f1.append(fighters[0].text)
+            f2.append(fighters[1].text)
+            winner.append(fighters[2].text)
     return None
 
-#preprocessing
-# remove rows where DOB is null
-# impute stance as orthodox for missing stances
 def create_df():
-    #create empty dataframe
+    
+    # creating dataframe
     df = pd.DataFrame()
-    
-    #creating the df
-    df["NAME"] = f_name
-    df["DOB"] = dob
-    df["RECORD"] = record
-    df["LF"] = last_fought
-    df["STANCE"] = stance
-    df["Height"] = height
-    df["Weight"] = weight
-    df["REACH"] = reach
-    df["SLPM"] = slpm
-    df["SAPM"] = sapm
-    df["STRA"] = stra
-    df["STRD"] = strd
-    df["TD"] = td
-    df["TDA"] = tda
-    df["TDD"] = tdd
-    df["SUBA"] = suba
-    
+    df["Events"] = events
+    df["Location"] = location
+    df["Fighter1"] = f1
+    df["Fighter2"] = f2
+    df["Winner"] = winner
+    df["fighter1_odds"] = f1_odds
+    df["fighter2_odds"] = f2_odds
+    df["Favourite"] = favourite
+    df["Label"] = label
+    print(f"Successfully scraped {df.shape[0]} fights and last fight card was {df.iloc[-1, :]['Events']} {df.iloc[-1, :]['Location']}")
+    print(df["Label"].value_counts()/len(df))
     
     return df
 
-def weightclass(df):
-    if df["Weight"] <= 115:
-        return "strawweight"
-    elif df["Weight"] <= 125:
-        return "flyweight"
-    elif df["Weight"] <= 135:
-        return "bantamweight"
-    elif df["Weight"] <= 145:
-        return "featherweight"
-    elif df["Weight"] <= 155:
-        return "lightweight"
-    elif df["Weight"] <= 170:
-        return "welterweight"
-    elif df["Weight"] <= 185:
-        return "middleweight"
-    elif df["Weight"] <= 205:
-        return "lightheavyweight"
+# functions to compute deltas
+
+def odds_delta(df):
+    if df["Favourite"] == "f1":
+        return df["fighter1_odds"] - df["fighter2_odds"]
     else:
-        return "heavyweight"
+        return df["fighter2_odds"] - df["fighter1_odds"]
 
-def age(df):
-    year_now = int(datetime.datetime.now().strftime("%Y")) 
-    year_born = int(df["DOB"].split(", ")[1])
-    return (year_now - year_born)
+def reach_delta(df):
+    if df["Favourite"] == "f1":
+        return df["REACH_x"] - df["REACH_y"]
+    else:
+        return df["REACH_y"] - df["REACH_x"]
 
-def year_lf(df):
-    year_now = int(datetime.datetime.now().strftime("%Y")) 
-    year_last_fought = int(df["LF"].split(", ")[1])
-    return (year_now - year_last_fought)
+def slpm_delta(df):
+    if df["Favourite"] == "f1":
+        return df["SLPM_x"] - df["SLPM_y"]
+    else:
+        return df["SLPM_y"] - df["SLPM_x"]
+
+def sapm_delta(df):
+    if df["Favourite"] == "f1":
+        return df["SAPM_x"] - df["SAPM_y"]
+    else:
+        return df["SAPM_y"] - df["SAPM_x"]
     
-def preprocessing(df):
-    # identifying NaNs
-    df = df.replace('--',np.nan)
-    df = df.replace('',np.nan)
+def stra_delta(df):
+    if df["Favourite"] == "f1":
+        return df["STRA_x"] - df["STRA_y"]
+    else:
+        return df["STRA_y"] - df["STRA_x"]
     
-    # removing useless rows
-    df = df[df["DOB"].notnull()]
-    df = df[df["Height"].notnull()]
-    df = df[df["LF"].notnull()]
+def strd_delta(df):
+    if df["Favourite"] == "f1":
+        return df["STRD_x"] - df["STRD_y"]
+    else:
+        return df["STRD_y"] - df["STRD_x"]
     
-    # imputing missing values
-    # Stance column - impute with Orthodox since most fighters are orthodox
-    df["STANCE"] = df["STANCE"].fillna("Orthodox")
+def td_delta(df):
+    if df["Favourite"] == "f1":
+        return df["TD_x"] - df["TD_y"]
+    else:
+        return df["TD_y"] - df["TD_x"]
+
+def tda_delta(df):
+    if df["Favourite"] == "f1":
+        return df["TDA_x"] - df["TDA_y"]
+    else:
+        return df["TDA_y"] - df["TDA_x"]
     
-    # create weightclass column
-    df["WeightClass"] = df.apply(weightclass, axis=1)
+def tdd_delta(df):
+    if df["Favourite"] == "f1":
+        return df["TDD_x"] - df["TDD_y"]
+    else:
+        return df["TDD_y"] - df["TDD_x"]
+
+def suba_delta(df):
+    if df["Favourite"] == "f1":
+        return df["SUBA_x"] - df["SUBA_y"]
+    else:
+        return df["SUBA_y"] - df["SUBA_x"]
+
+def age_delta(df):
+    if df["Favourite"] == "f1":
+        return df["Age_x"] - df["Age_y"]
+    else:
+        return df["Age_y"] - df["Age_x"]
+
+
+def merge_data(df):
     
-    # Reach column - impute with mean of respective weightclass
-    df["REACH"] = df.groupby("WeightClass")['REACH'].apply(lambda x: x.fillna(round(x.mean(), 1)))
+    # We're always asking for json because it's the easiest to deal with
+    morph_api_url = "https://api.morph.io/Dennis-Cao/dc_ufc_fighters_db/data.json"
+
+    # Keep this key secret using morph secret variables
+    morph_api_key = os.environ['MORPH_API_KEY']
+
+    r = requests.get(morph_api_url, params={
+      'key': morph_api_key,
+      'query': "select * from data"
+    })
+
+    j = r.json()
     
-    # create age column
-    df["Age"] = df.apply(age, axis=1)
+    # fighters db dataset to me merged
+    fighters_db = pd.DataFrame.from_dict(j)
     
-    # create year since last fought
-    df["Year_LF"] = df.apply(year_lf, axis=1)
+    test = pd.merge(df, fighters_db, left_on=["Fighter1"], right_on=["NAME"])
+    test2 = pd.merge(test, fighters_db, left_on=["Fighter2"], right_on=["NAME"])
     
-    # created active or inactive flag
-    df["Active"] = df["Year_LF"].apply(lambda x: True if x <= 4 else False)
+    test2["Odds_delta"] = test2.apply(odds_delta, axis=1)
+    test2["REACH_delta"] = test2.apply(reach_delta, axis=1)
+    test2["SLPM_delta"] = test2.apply(slpm_delta, axis=1)
+    test2["SAPM_delta"] = test2.apply(sapm_delta, axis=1)
+    test2["STRA_delta"] = test2.apply(stra_delta, axis=1)
+    test2["STRD_delta"] = test2.apply(strd_delta, axis=1)
+    test2["TD_delta"] = test2.apply(td_delta, axis=1)
+    test2["TDA_delta"] = test2.apply(tda_delta, axis=1)
+    test2["TDD_delta"] = test2.apply(tdd_delta, axis=1)
+    test2["SUBA_delta"] = test2.apply(suba_delta, axis=1)
+    test2["AGE_delta"] = test2.apply(age_delta, axis=1)
     
-    return df
+    final_df = test2[['Events', 'Location', 'Fighter1', 'Fighter2', 'Favourite', 'Label', 'REACH_delta', 'SLPM_delta', 'SAPM_delta', 'STRA_delta', 'STRD_delta', 'TD_delta', 'TDA_delta', 'TDD_delta', 'SUBA_delta', "AGE_delta", 'Odds_delta']]
+    
+    return final_df
 
 scrape_data()
 df = create_df()
-df = preprocessing(df)
-print("Scraping completed")
+df = merge_data(df)
 
 conn = sqlite3.connect('data.sqlite')
 df.to_sql('data', conn, if_exists='replace')
-print('Db successfully constructed and saved')
+print('Fights Merged Db successfully constructed and saved')
 conn.close()
